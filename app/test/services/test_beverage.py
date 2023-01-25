82b8d0ae4edd8b4de903dab33fa5a4986be8ae64
David Cadena <david.cadena@hotmail.com>
2023-01-25 13:56:17 -0500

    add tests to evaluate beverage services and controllers

---
+++ app/test/services/test_beverage.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_create_beverage_service(create_beverage):
+    beverage = create_beverage.json
+    pytest.assume(create_beverage.status.startswith('200'))
+    pytest.assume(beverage['_id'])
+    pytest.assume(beverage['name'])
+    pytest.assume(beverage['price'])
+
+def test_get_beverages_service(client, create_beverages, beverage_uri):
+    response = client.get(beverage_uri)
+    pytest.assume(response.status.startswith('200'))
+    returned_beverages = {
+        beverage['_id']: beverage for beverage in response.json}
+    for beverage in create_beverages:
+        pytest.assume(beverage['_id'] in returned_beverages)
