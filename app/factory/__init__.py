281c87f12bc5f19741daa6147164ef6a31b32b76
David Cadena <david.cadena@hotmail.com>
2023-02-26 23:43:50 -0500

    remove unnecessary code

--- app/factory/__init__.py
+++
@@ -1,5 +0,0 @@
-from .interfaz import AbstractReport
-from .factory import ReportFactory
-from .customerReport import CustomerReport
-from .ingredientReport import IngredientReport
-from .monthReport import MonthReport
